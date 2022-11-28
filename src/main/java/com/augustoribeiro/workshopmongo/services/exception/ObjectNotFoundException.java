c613a87fffbfffcf79158a37bffdd325d6709331
Augusto <augustoribeiro100.g.j@gmail.com>
2022-11-28 20:20:01 -0300

    Obtendo um usuário por id

---
+++ src/main/java/com/augustoribeiro/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,10 @@
+package com.augustoribeiro.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+
+}
